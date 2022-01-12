df3bed89f8b3e0486a5f56561003fdae7057de1d
daria <siedykhdaria@gmail.com>
2022-01-12 22:04:19 +0200

    add video-section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-// Test import of a JavaScript function
-import {example} from './js/example'
 
-// Test import of an asset
-// import webpackLogo from './images/webpack-logo.svg'
-
+import './js/main'
 // Test import of styles
 import './styles/index.scss'
 console.log('run js',)
---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+
+/* ================= VIDEO ========================= */
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#story-video-overlay');
+const videoFile = document.querySelector('#video-story');
+
+let isClicked = false;
+
+function toggleOverlay(event){
+
+    if (event.type === 'mouseleave') {
+        videoOverlay.classList.add('overlay-hidden');
+    } else {
+        videoOverlay.classList.remove('overlay-hidden');
+    }
+}
+
+videoOverlay.onmouseleave = toggleOverlay;
+videoOverlay.onmouseenter = toggleOverlay;
+
+videoBtn.addEventListener('click', function () {
+    if (videoFile.paused) {
+        videoOverlay.classList.add('overlay-hidden');
+        videoFile.play();
+        isClicked = true;
+        return;
+    }
+
+    if (!videoFile.paused && isClicked) {
+        videoBtnIcon.src = "assets/images/story/pause-white.png";
+        videoOverlay.classList.remove('overlay-hidden');
+        videoFile.pause();
+        return;
+    }
+
+
+    if (!videoFile.paused) {
+        videoFile.pause();
+        videoOverlay.classList.remove('overlay-hidden');
+        videoBtnIcon.src = 'assets/images/story/play-white.svg';
+    }
+})
